e28e55d464dacb0bf6a8399b14f80e86594d8e54
ADMIN <ADMIN@DESKTOP-M57JVOT>
2026-09-25 02:17:02 +0530

    code update

--- app/main.py
+++ app/main.py
@@ -1,18 +1,14 @@
-from mcp.server import MCPServer
+from mcp.server.fastmcp import FastMCP
 
 from tools.weather_tools import register_weather_tools
 from tools.order_tools import register_order_tools
 from tools.product_tools import register_product_tools
 
-
-
 from resources.business_resources import register_resources
 from prompts.business_prompts import register_prompts
 
 
-mcp = MCPServer(
-    "Enterprise TANDMS MCP Server"
-)
+mcp = FastMCP("Enterprise TANDMS MCP Server")
 
 
 def register_all_components():
@@ -33,9 +29,4 @@ register_all_components()
 
 
 if __name__ == "__main__":
-
-    mcp.run(
-        transport="streamable-http",
-        host="127.0.0.1",
-        port=8000
-    )
+    mcp.run(transport="streamable-http")
